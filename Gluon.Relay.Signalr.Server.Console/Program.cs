39b6b0953b5616c6cca4af7afb5b77f649a81c2e
Greg Hoover <greg@calara.net>
2017-10-05 21:18:42 -0400

    - Create empty projects.

---
+++ Gluon.Relay.Signalr.Server.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gluon.Relay.Signalr.Server.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Gluon.Tester.Server.Conosle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gluon.Tester.Server.Conosle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
