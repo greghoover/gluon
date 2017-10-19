276a503e2d8b4f7c2354d2abcc1d7d03df997051
Greg Hoover <greg@calara.net>
2017-10-19 17:40:22 -0400

    Eliminate the untyped Execute method by moving it to a new base class, RequestResponseServiceBase, to remove the noise from all service class.

--- Gluon.Tester.Server.Library/FileSystemQueryService.cs
+++ Gluon.Tester.Server.Library/FileSystemQueryService.cs
@@ -1,15 +1,14 @@
 ﻿using Gluon.Relay.Contracts;
 using Gluon.Tester.Contracts;
-using Newtonsoft.Json.Linq;
-using System;
 using System.IO;
 
 namespace Gluon.Tester.Server.Library
 {
-    public class FileSystemQueryService : IServiceType, IServiceType<FileSystemQueryRqst, FileSystemQueryRspn>
+    public class FileSystemQueryService : RequestResponseServiceBase<FileSystemQueryRqst, FileSystemQueryRspn>, IServiceType<FileSystemQueryRqst, FileSystemQueryRspn>
     {
+        
         // todo: consider making this an async method.
-        public FileSystemQueryRspn Execute(FileSystemQueryRqst request)
+        public override FileSystemQueryRspn Execute(FileSystemQueryRqst request)
         {
             string responseText = null;
             switch (request.QueryType)
@@ -21,18 +20,6 @@ namespace Gluon.Tester.Server.Library
 
             var response = new FileSystemQueryRspn(request, responseText);
             return response;
-        }
-
-        // todo: Eliminate the untyped Execute method, or at least move it to
-        // a more centralized area. Don't want this noise in every service class.
-        public void Execute(ICommunicationClient hub, object request)
-        {
-            var json = request as JObject;
-            var rqst = json.ToObject<FileSystemQueryRqst>();
-            //return Execute(hub, rqst);
-            var response = Execute(rqst);
-            Console.WriteLine(response);
-            hub.InvokeAsync(CX.RelayResponseMethodName, response.CorrelationId, response).Wait();
         }
     }
 }
---
+++ Gluon.Tester.Server.Library/RequestResponseServiceBase.cs
@@ -0,0 +1,23 @@
+﻿using Gluon.Relay.Contracts;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Gluon.Tester.Server.Library
+{
+    public abstract class RequestResponseServiceBase<TRequest, TResponse> : IServiceType
+        where TRequest : RelayMessageBase
+        where TResponse : RelayResponseBase<TRequest>
+    {
+        public void Execute(ICommunicationClient hub, object request)
+        {
+            var json = request as JObject;
+            var rqst = json.ToObject<TRequest>();
+            //return Execute(hub, rqst);
+            var response = Execute(rqst);
+            Console.WriteLine(response);
+            hub.InvokeAsync(CX.RelayResponseMethodName, response.CorrelationId, response).Wait();
+        }
+
+        public abstract TResponse Execute(TRequest rqst);
+    }
+}
