ffb2f4d0031bc83e863cc491d0a403475343ca36
Greg Hoover <greg@calara.net>
2018-03-05 00:44:17 -0500

    test

--- hbar/hbar.Contract/Credits.cs
+++ hbar/hbar.Contract/Credits.cs
@@ -3,6 +3,6 @@
 // Solution Name
 // https://www.theguardian.com/science/2013/nov/10/what-is-heisenbergs-uncertainty-principle
 
-// Protocol Buffers
+// Protocol Buffers  
 // https://johnkoerner.com/csharp/IPC-in-net-core-using-protobuf/
 // https://github.com/johnkoerner/SimpleIPCDotNetCore/tree/master/Client
