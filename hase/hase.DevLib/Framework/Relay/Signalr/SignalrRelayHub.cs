e6fbe42754ccd6b40b5da6572e96af7c97019580
Greg Hoover <228446+greghoover@users.noreply.github.com>
2018-05-14 01:10:35 -0400

    Refactor SignalR relay hub, break down into smaller sections to allow in anticipation of additional refactoring.

--- hase/hase.DevLib/Framework/Relay/Signalr/SignalrRelayHub.cs
+++ hase/hase.DevLib/Framework/Relay/Signalr/SignalrRelayHub.cs
@@ -1,7 +1,5 @@
-﻿using hase.DevLib.Framework.Contract;
-using hase.DevLib.Framework.Service;
+﻿using hase.DevLib.Framework.Service;
 using Microsoft.AspNetCore.SignalR;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
@@ -32,15 +30,7 @@ namespace hase.DevLib.Framework.Relay.Signalr
         }
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            var item = DispatcherConnections.Where(kvp => kvp.Value == Context.ConnectionId)
-                 .Select(kvp => new { kvp.Key, kvp.Value })
-                 .FirstOrDefault();
-            
-            if (item != null)
-            {
-                var dummy = default(string);
-                DispatcherConnections.TryRemove(item.Key, out dummy);
-            }
+            UnRegisterServiceDispatcherAsync(Context.ConnectionId);
             return base.OnDisconnectedAsync(exception);
         }
 
@@ -48,79 +38,108 @@ namespace hase.DevLib.Framework.Relay.Signalr
         {
             var connectionId = Context.ConnectionId;
             DispatcherConnections.AddOrUpdate(dispatcherChannel, connectionId, (key, val) => { return val; });
-            Console.WriteLine($"srrs:Dispatcher [{dispatcherChannel}] connection registered.");
+            Console.WriteLine($"srrs:Registered dispatcher [{dispatcherChannel}].");
+            return Task.CompletedTask;
+        }
+        private Task UnRegisterServiceDispatcherAsync(string connectionId)
+        {
+            var dispatcherChannel = this.GetConnectionDispatcherChannel(connectionId);
+            if (dispatcherChannel != null)
+            {
+                var dummy = default(string);
+                if (DispatcherConnections.TryRemove(dispatcherChannel, out dummy))
+                    Console.WriteLine($"srrs:UnRegistered dispatcher [{dispatcherChannel}].");
+
+            }
             return Task.CompletedTask;
         }
 
-        //public async Task<object> ProcessProxyRequestAsync(string proxyChannel, object req)
         public async Task<HttpResponseMessageWrapperEx> ProcessProxyRequestAsync(HttpRequestMessageWrapperEx request)
         {
-            var proxyChannel = request.GetSourceChannel();
             var requestId = request.GetRequestId();
+            var proxyChannel = request.GetSourceChannel();
+            var dispatcherChannel = ServiceTypesUtil.GetProxyServiceName(proxyChannel); // simpleton routing
 
-            var connectionId = Context.ConnectionId;
-            ProxyRequests.AddOrUpdate(requestId, connectionId, (key, val) => { return val; });
-            Console.WriteLine($"srrs:Proxy [{proxyChannel}] connected and request [{requestId}] received.");
+            Console.WriteLine($"srrs:Request [{requestId}] received on proxy channel [{proxyChannel}].");
+            ProxyRequests.AddOrUpdate(requestId, Context.ConnectionId, (key, val) => { return val; });
 
             if (_cts.IsCancellationRequested)
                 return null;
-            var dispatcherChannel = ServiceTypesUtil.GetProxyServiceName(proxyChannel);
+            await ForwardRequestToDispatcher(requestId, request, dispatcherChannel);
+
+            if (_cts.IsCancellationRequested)
+                return null;
+            var response = await AwaitResponseFromDispatcher(requestId, dispatcherChannel);
+            return response;
+        }
+
+        private async Task ForwardRequestToDispatcher(string requestId, HttpRequestMessageWrapperEx request, string dispatcherChannel)
+        {
             Console.WriteLine($"srrs:Sending request to dispatcher [{dispatcherChannel}] [{requestId}].");
             var dispatcherConnectionId = GetDispatcherConnectionId(dispatcherChannel);
             await Clients.Client(dispatcherConnectionId).SendAsync("dispatch", request);
 
-            if (_cts.IsCancellationRequested)
-                return null;
-            Console.WriteLine($"srrs:Awaiting response from dispatcher [{dispatcherChannel}].");
-
-            HttpResponseMessageWrapperEx response = null;
-            while (true)
-            {
-                if (_cts.IsCancellationRequested)
-                    return null;
-
-                Task.Delay(100).Wait(); // why?
-                if (DispatcherResponses.TryRemove(requestId, out response))
-                    break;
-            }
-
-            return response;
+            return;
         }
-
-
         public Task DispatcherResponseAsync(string dispatcherChannel, string requestId, HttpResponseMessageWrapperEx response)
         {
             Console.WriteLine($"srrs:Received message from dispatcher [{dispatcherChannel}].");
             DispatcherResponses.AddOrUpdate(requestId, response, (key, val) => { return val; });
             return Task.CompletedTask;
         }
+        private async Task<HttpResponseMessageWrapperEx> AwaitResponseFromDispatcher(string requestId, string dispatcherChannel)
+        {
+            Console.WriteLine($"srrs:Awaiting response from dispatcher [{dispatcherChannel}].");
+            HttpResponseMessageWrapperEx response = null;
+            while (true)
+            {
+                if (_cts.IsCancellationRequested)
+                    return null;
 
+                await Task.Delay(150);
+                if (DispatcherResponses.TryRemove(requestId, out response))
+                    break;
+            }
+            return response;
+        }
 
         public async Task StartAsync()
         {
-            await Task.CompletedTask; // no compiler warning please
-
-            var ct = _cts.Token;
-
+            await Task.Delay(1); // hush compile warning
             Console.WriteLine($"srrs:Listening for dispatcher connections.");
-            //await ListenForServiceConnectionAsync(ct); // obsolete processing model
             Console.WriteLine($"srrs:Listening for proxy connections.");
-            // await proxy connections // obsolete processing model
         }
         public async Task StopAsync()
         {
-            _cts.Cancel();
-            await Task.Delay(1000); // time to clean up
-            _cts.Dispose();
+            try
+            {
+                await Task.Delay(1); // hush compile warning
+                if (_cts != null)
+                {
+                    _cts.Cancel();
+                    Task.Delay(1000).Wait(); // time to clean up
+                }
+            }
+            finally
+            {
+                if (_cts != null)
+                    _cts.Dispose();
+            }
         }
 
         private string GetDispatcherConnectionId(string dispatcherChannel)
         {
             var dispatcherConnectionId = default(string);
             if (!DispatcherConnections.TryGetValue(dispatcherChannel, out dispatcherConnectionId))
-                throw new ApplicationException($"No dispatcher [{dispatcherChannel}] available to handle request.");
+                throw new ApplicationException($"Could not retrieve connection id for unregistered dispatcher channel [{dispatcherChannel}] .");
 
             return dispatcherConnectionId;
+        }
+        private string GetConnectionDispatcherChannel(string connectionId)
+        {
+            return DispatcherConnections.Where(kvp => kvp.Value == connectionId)
+                 .Select(kvp => new { kvp.Key, kvp.Value })
+                 .FirstOrDefault()?.Key;
         }
     }
 }
