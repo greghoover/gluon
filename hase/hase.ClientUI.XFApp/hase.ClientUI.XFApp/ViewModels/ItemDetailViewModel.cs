1994f8b4b3449b831f9b3a0dda7130317fafe4a2
Greg Hoover <228446+greghoover@users.noreply.github.com>
2018-05-26 15:10:36 -0400

    Clear out extraneous stuff.

---
+++ hase/hase.ClientUI.XFApp/hase.ClientUI.XFApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using hase.ClientUI.XFApp.Models;
+
+namespace hase.ClientUI.XFApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
--- hase/hase.ServiceApp.XFHost/hase.ServiceApp.XFHost/MainPage.xaml.cs
+++ hase/hase.ServiceApp.XFHost/hase.ServiceApp.XFHost/MainPage.xaml.cs
@@ -1,76 +1,31 @@
-﻿using hase.DevLib.Framework.Relay;
-using hase.DevLib.Framework.Relay.Signalr;
-using hase.DevLib.Services.Calculator.Contract;
-using hase.DevLib.Services.Calculator.Service;
+﻿using hase.DevLib.Framework.Relay.Signalr;
 using hase.DevLib.Services.FileSystemQuery.Client;
 using hase.DevLib.Services.FileSystemQuery.Contract;
-using hase.DevLib.Services.FileSystemQuery.Service;
-using Microsoft.AspNetCore.SignalR.Client;
 using System;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace hase.ServiceApp.XFHost
 {
     public partial class MainPage : ContentPage
 	{
-		private IRelayDispatcher<FileSystemQueryService, FileSystemQueryRequest, FileSystemQueryResponse> fsqDispatcher = null;
-		private IRelayDispatcher<CalculatorService, CalculatorRequest, CalculatorResponse> calcDispatcher = null;
-
 		public MainPage()
 		{
 			InitializeComponent();
         }
         private void PressMeButton_Pressed(object sender, EventArgs e)
         {
-            (sender as Button).Text = "9";
+            (sender as Button).Text = "Pressed.";
         }
 
-        private async void PressMeButton_Clicked(object sender, EventArgs e)
+        private void PressMeButton_Clicked(object sender, EventArgs e)
         {
             var button = (Button)sender;
-
             button.Text = "Init";
-            //Task.Run(() =>
-            //{
-            HubConnection _hub = null;
-
-            //try
-            //{
-            //    //button.Text = "Building...";
-            //    //Task.Delay(1500).Wait();
-            //    _hub = new HubConnectionBuilder()
-            //        .WithUrl("http://localhost:5000/route")
-            //        .Build();
-            //    //button.Text = "Built";
-            //    //Task.Delay(1500).Wait();
-
-            //    //button.Text = "Connecting...";
-            //    //Task.Delay(1500).Wait();
-            //    await _hub.StartAsync();
-            //    //button.Text = "Connected";
-            //    //Task.Delay(1500).Wait();
-            //}
-            //catch (Exception ex)
-            //{
-            //    var txt = ex.Message;
-            //    if (ex.InnerException != null)
-            //        txt += Environment.NewLine + ex.InnerException.Message;
-            //    //button.Text = txt;
-            //    //Task.Delay(1500).Wait();
-            //}
-            //finally
-            //{
-            //    //button.Text = "Disposing...";
-            //    await _hub.DisposeAsync();
-            //    //button.Text = "Disposed";
-            //}
 
             try
             {
                 var folderPath = @"c:\";
                 var fsq = new FileSystemQuery(typeof(SignalrRelayProxy<FileSystemQueryRequest, FileSystemQueryResponse>));
-                //var fsq = new FileSystemQuery(typeof(NamedPipeRelayProxy<FileSystemQueryRequest, FileSystemQueryResponse>));
                 var result = fsq.DoesDirectoryExist(folderPath);
                 button.Text = $"Does folder [{folderPath}] exist? [{result}].";
             }
@@ -84,8 +39,6 @@ namespace hase.ServiceApp.XFHost
             finally
             {
             }
-
-            //}).Wait();
 
         }
     }
