a3bcf842a70f45c98aab88a7ca2a8261471c0b78
Greg Hoover <228446+greghoover@users.noreply.github.com>
2018-06-17 16:37:07 -0400

    Remove old file system query folder. Should have does this with the previous check-in.

--- hase/hase.DevLib/Services/FileSystemQuery/Contract/IFileSystemQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using hase.DevLib.Framework.Contract;
-
-namespace hase.DevLib.Services.FileSystemQuery.Contract
-{
-    public interface IFileSystemQuery : IServiceClient<FileSystemQueryRequest, FileSystemQueryResponse>
-    {
-        bool? DoesDirectoryExist(string folderPath);
-    }
-}
